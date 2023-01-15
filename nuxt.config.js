9373630159ddc1a8262716e507a9147ec56dd112
Nadiia Palaida <n.palaida@gmail.com>
2023-01-15 23:04:44 +0200

    update

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,5 @@
 export default {
-  ssr: false,
+   /* ssr: false,*/
   target: 'static',
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
--- store/index.js
+++ store/index.js
@@ -1,5 +1,6 @@
 export const actions = {
   nuxtServerInit({ dispatch }) {
+    console.log('nuxtServerInit')
     dispatch('movies/getAllMovies')
   }
 }
